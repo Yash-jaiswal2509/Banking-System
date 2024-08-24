086ae7d32e7d61119fb00c87daa138b6cb02fdb0
Yash-jaiswal2509 <yashjaiswal2509@gmail.com>
2024-08-24 22:51:01 +0530

    (client): doughnut chart updated

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -6,7 +6,7 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
   const accountNames = accounts.map((account) => account.name);
-  const balances = accounts.map((account) => account.currentBalance);
+  const balances = accounts.map((account) => account.currentBalance * 85);
   const data = {
     datasets: [
       {
@@ -15,7 +15,7 @@ const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
         backgroundColor: ["#0747b6", "#2265d8", "#2f91fa"],
       },
     ],
-    labels: ["SBI", "HDFC", "ICICI"],
+    labels: accountNames,
   };
   return (
     <Doughnut
